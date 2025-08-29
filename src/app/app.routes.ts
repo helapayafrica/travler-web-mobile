f1ddd5237881cd57398723b7371bef84c3e69f4a
Samuel F. W. Ndegwa <s.f.waweru@gmail.com>
2025-08-29 16:37:33 +0300

    init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { NotFound } from './views/not-found/not-found';
+import { SignUp } from './views/Auth/sign-up/sign-up';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: 'sign-up',
+        component: SignUp
+    },
+    
+    {
+        path: 'login',
+        component: SignUp
+    },
+    
+    {
+        path: '**',
+        component : NotFound
+    }
+
+];
