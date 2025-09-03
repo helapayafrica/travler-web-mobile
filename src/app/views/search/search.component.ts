c3ee9720a5635bfabea97cb7bf9c9188eb97269a
Samuel F. W. Ndegwa <s.f.waweru@gmail.com>
2025-09-03 13:48:18 +0300

    Added the Filte and Sort Drawer

--- src/app/views/search/search.component.ts
+++ src/app/views/search/search.component.ts
@@ -3,13 +3,36 @@ import {SearchComponent as SharedSearchComponent} from '../../shared/search/sear
 import {ResultsComponent} from './sections/results/results.component';
 import {BackendService} from '../../services/backend';
 import {BookingService} from '../../services/booking';
+import {Breadcrumb} from 'primeng/breadcrumb';
+import {RouterLink} from '@angular/router';
+import {PrimeTemplate} from 'primeng/api';
+import {CurrencyPipe, NgForOf, NgIf} from '@angular/common';
+import {Button, ButtonDirective} from 'primeng/button';
+import {Drawer} from 'primeng/drawer';
+import {Tab, TabList, TabPanel, TabPanels, Tabs} from 'primeng/tabs';
+import {NgxSliderModule} from '@angular-slider/ngx-slider';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-view-search',
   standalone: true,
   imports: [
     SharedSearchComponent,
-    ResultsComponent
+    ResultsComponent,
+    Breadcrumb,
+    PrimeTemplate,
+    NgIf,
+    Button,
+    Drawer,
+    TabPanel,
+    NgForOf,
+    NgxSliderModule,
+    Tabs,
+    TabList,
+    Tab,
+    CurrencyPipe,
+    FormsModule,
+    TabPanels
   ],
   templateUrl: './search.component.html',
   styleUrl: './search.component.scss'
@@ -28,5 +51,44 @@ async getPayload(){
     this.buses=res.data
   })
 }
+//BreadCrumb
+  home = { label: 'Home' };
+  items: any[] = [{ text: 'Pune to Bhopal' }];
+  isFormOpen = true;
+
+  filterVisible = false
+  toggleForm() {
+    console.log("Toggle is clicked!")
+    console.log(this.isFormOpen)
+    this.isFormOpen = !this.isFormOpen;
+    console.log(this.isFormOpen)
+  }
+// a the filter setting for Drawer
+  minValue = 0;
+  maxValue = 10000;
+  options: any = {
+    floor: 0,
+    ceil: 10000,
+    step: 100,
+    translate: (value: number): string => {
+      return `KES ${value}`;
+    }
+  }
+
+  durations = [
+    { label: '3 hours', value: '3h', icon: 'bi bi-clock' },
+    { label: '3-5 hours', value: '3-5h', icon: 'bi bi-hourglass-split' },
+    { label: '5-7 hours', value: '5-7h', icon: 'bi bi-stopwatch' },
+    { label: '7+ hours', value: '7+h', icon: 'bi bi-alarm' }
+  ];
+  selectedDuration = '3h';
+
+  services = [
+    { label: 'Wi-Fi', checked: false, icon: 'bi bi-wifi' },
+    { label: 'TV', checked: false, icon: 'bi bi-tv' },
+    { label: 'Bed', checked: false, icon: 'bi bi-moon' },
+    { label: 'Meals', checked: false, icon: 'bi bi-egg-fried' }
+  ];
+
 
 }
