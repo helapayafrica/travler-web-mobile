823e53e24b478bed7b9d254610609a27734a4f27
Samuel F. W. Ndegwa <s.f.waweru@gmail.com>
2025-10-03 12:05:50 +0300

    Fix: fix privacy policy and view seats btn bug.

--- src/app/views/privacy/privacy.ts
+++ src/app/views/privacy/privacy.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import {TranslatePipe} from '@ngx-translate/core';
+import {NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-privacy',
   imports: [
-    TranslatePipe
+    TranslatePipe,
   ],
   templateUrl: './privacy.html',
   styleUrl: './privacy.scss'
